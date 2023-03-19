7b0aec4b4fbd86c1fd15169baa2ba36e50dd7a88
csennn <120573618+csennn@users.noreply.github.com>
2023-03-19 21:18:48 +0300

    Add files via upload

---
+++ localedatexample.java
@@ -0,0 +1,19 @@
+package bos;
+
+import java.time.LocalDate;
+
+public class LocalDateExample {
+
+	public static void main(String[] args) {
+
+		// to print the current date
+		LocalDate localDate = LocalDate.now();
+		System.out.println(localDate);
+
+		// to print the 256th day of 2023
+		localDate = LocalDate.ofYearDay(2023, 256);
+		System.out.println(localDate); // 2023-09-13
+
+	}
+
+}
